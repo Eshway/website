7abb562936a411253c228cd676397cf54946aa66
Eshu Koli <eshller@Eshus-MacBook-Air.local>
2023-10-29 20:15:00 +0530

    analytics

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import Script from 'next/script';
 import { Footer, Navbar } from '../components';
 import { About, Explore, Services, GetStarted, Hero, WhatsNew, Merch } from '../sections';
 import EshVision from '../sections/EshVision';
 
 const Home = () => (
   <div className="bg-primary-black overflow-hidden">
+    <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
+    <Script id="google-analytics">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-3MY67GFCTB');
+        `}
+    </Script>
     <Navbar />
     <Hero />
     <div className="relative">
