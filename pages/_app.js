1930f6b54665ab9dfed1a47ae71a5f06145e52c3
Eshu Koli <eshller@Eshus-MacBook-Air.local>
2023-10-29 22:48:07 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import Head from "next/head";
+import Head from 'next/head';
 
-import "../styles/globals.css";
-import Script from "next/script";
+import '../styles/globals.css';
+import Script from 'next/script';
 
 const MyApp = ({ Component, pageProps }) => (
   <>
