0536b6a5bd706a12184db20327ed048be0585d01
Eshu Koli <eshller@Eshus-MacBook-Air.local>
2024-04-08 19:03:34 +0530

    bid adieu Nakshita

--- constants/index.js
+++ constants/index.js
@@ -1,86 +1,75 @@
+/* eslint-disable quotes */
 export const exploreWorlds = [
   {
-    id: '1',
-    imgUrl: '/E_nobg.webp',
-    title: 'Eshller',
-    pos: 'Founder',
+    id: "1",
+    imgUrl: "/E_nobg.webp",
+    title: "Eshller",
+    pos: "Founder",
   },
   {
-    id: '2',
-    imgUrl: '/L_nobg.webp',
-    title: 'Likhitha',
-    pos: 'Core Team',
+    id: "2",
+    imgUrl: "/L_nobg.webp",
+    title: "Likhitha",
+    pos: "Core Team",
   },
   {
-    id: '3',
-    imgUrl: '/V_nobg.webp',
-    title: 'Vallabh',
-    pos: 'Core Team',
+    id: "3",
+    imgUrl: "/V_nobg.webp",
+    title: "Vallabh",
+    pos: "Core Team",
   },
   {
-    id: '4',
-    imgUrl: '/A_nobg.webp',
-    title: 'Akshat',
-    pos: 'Core Team',
+    id: "4",
+    imgUrl: "/A_nobg.webp",
+    title: "Akshat",
+    pos: "Core Team",
   },
-  {
-    id: '5',
-    imgUrl: '/nakshita.webp',
-    title: 'Nakshita',
-    pos: 'Core Team',
-  },
-
 ];
 
 export const startingFeatures = [
-  'Contact the team by filling the form',
-  'Get quotation and the prototype',
-  'Your product will be ready to deliever in the shortest span possible',
+  "Contact the team by filling the form",
+  "Get quotation and the prototype",
+  "Your product will be ready to deliever in the shortest span possible",
 ];
 
-export const images = [
-  '/Front.webp',
-  '/tshirtmockup.webp',
-];
+export const images = ["/Front.webp", "/tshirtmockup.webp"];
 
 export const productDesc = [
-  'Free Shipping',
+  "Free Shipping",
   "Tech Enthusiast's Essential: Trendy Shirts with a Techie Twist",
   // 'Experience Unmatched Comfort with Our Cotton Tech-Inspired T-Shirts!',
 ];
 export const newFeatures = [
   {
-    imgUrl: '/dollar.png',
-    title: 'Not satisfied with the service?',
-    subtitle:
-        'You can come back to us for infinite revisions at free of cost*',
+    imgUrl: "/dollar.png",
+    title: "Not satisfied with the service?",
+    subtitle: "You can come back to us for infinite revisions at free of cost*",
   },
   {
-    imgUrl: '/time.png',
-    title: 'High-end service at low prices',
-    subtitle:
-        'Get high-end service at extremely reasonable competitive prices',
+    imgUrl: "/time.png",
+    title: "High-end service at low prices",
+    subtitle: "Get high-end service at extremely reasonable competitive prices",
   },
 ];
 
 export const insights = [
   {
-    imgUrl: '/web.png',
-    title: 'Web Development',
+    imgUrl: "/web.png",
+    title: "Web Development",
     subtitle:
-        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+      "Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.",
   },
   {
-    imgUrl: '/planet-07.png',
-    title: '7 tips to easily master the madness of the Metaverse',
+    imgUrl: "/planet-07.png",
+    title: "7 tips to easily master the madness of the Metaverse",
     subtitle:
-        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+      "Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum",
   },
   {
-    imgUrl: '/planet-08.png',
-    title: 'With one platform you can explore the whole world virtually',
+    imgUrl: "/planet-08.png",
+    title: "With one platform you can explore the whole world virtually",
     subtitle:
-        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+      "Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem",
   },
 ];
 
@@ -91,14 +80,14 @@ export const socials = [
   //   link: 'http://twitter.com',
   // },
   {
-    name: 'linkedin',
-    url: '/linkedin.svg',
-    link: 'https://www.linkedin.com/company/eshway',
+    name: "linkedin",
+    url: "/linkedin.svg",
+    link: "https://www.linkedin.com/company/eshway",
   },
   {
-    name: 'instagram',
-    url: '/instagram.svg',
-    link: 'https://www.instagram.com/esh_way/',
+    name: "instagram",
+    url: "/instagram.svg",
+    link: "https://www.instagram.com/esh_way/",
   },
   // {
   //   name: 'facebook',
@@ -109,41 +98,96 @@ export const socials = [
 
 export const feedback = [
   {
-    id: 'feedback-1',
-    content:
-      'Web Development',
-    name: 'Web Dev',
-    title: 'Web Dev',
-    img: '/web.png',
+    id: "feedback-1",
+    content: "Web Development",
+    name: "Web Dev",
+    title: "Web Dev",
+    img: "/web.png",
   },
   {
-    id: 'feedback-2',
-    content:
-      'Application Development',
-    name: 'App Dev',
-    title: 'App Dev',
-    img: '/phone.png',
+    id: "feedback-2",
+    content: "Application Development",
+    name: "App Dev",
+    title: "App Dev",
+    img: "/phone.png",
   },
   {
-    id: 'feedback-3',
-    content:
-      'Search Engine Optimisation',
-    name: 'SEO',
-    title: 'SEO',
-    img: '/seo.png',
+    id: "feedback-3",
+    content: "Search Engine Optimisation",
+    name: "SEO",
+    title: "SEO",
+    img: "/seo.png",
   },
 ];
 
 export const eshvision = [
-  { id: '1', topic: 'Before we Start', deadline: '1st September', url: 'https://notion.io', testLink: 'google' },
-  { id: '2', topic: 'Introduction to HTML', deadline: '2nd September', url: 'https://notion.io', testLink: 'google' },
-  { id: '3', topic: 'Advanced HTML', deadline: '3rd September', url: 'https://notion.io', testLink: 'google' },
+  {
+    id: "1",
+    topic: "Before we Start",
+    deadline: "1st September",
+    url: "https://notion.io",
+    testLink: "google",
+  },
+  {
+    id: "2",
+    topic: "Introduction to HTML",
+    deadline: "2nd September",
+    url: "https://notion.io",
+    testLink: "google",
+  },
+  {
+    id: "3",
+    topic: "Advanced HTML",
+    deadline: "3rd September",
+    url: "https://notion.io",
+    testLink: "google",
+  },
 ];
 
 export const testimonials = [
-  { id: '1', name: 'Eknoor Singh', company: 'Speezy', logo: 'https://media.licdn.com/dms/image/C4D0BAQFypz5GTWfe0g/company-logo_200_200/0/1673103068185?e=1704326400&v=beta&t=nogzLKRMtVsgYX9Y0tpFQXBliZhKMhzUBQWte3af91w', pic: '/eknoor.png', star: '5', exp: 'Great work so far, loved the fact that Eshu personally took it up! Looking forward to the final product' },
-  { id: '2', name: 'Abhishek Pai Angle', company: '', logo: 'https://www.ribin.in/assets/SiteLogo-b873c477.png', pic: '/abhishek.png', star: '5', exp: 'Great work. They could exactly replicate the design into website and the speed was super quick. Eshu personally solved all the issue I had in quickest of time. Would definitely work with them again!' },
-  { id: '3', name: 'Sujas Jain', company: 'Abhyuday', logo: 'abhyuday', pic: '', star: '5', exp: 'Work execution was smooth and task was done on time!' },
-  { id: '4', name: 'Formula AshwaRiders', company: 'Formula Ashwariders', logo: 'https://formulaashwariders.in/assets/logo-e2d299f2.png', pic: 'https://raw.githubusercontent.com/Eshller/formulaashwariders/main/public/images/t7.jpeg', star: '5', exp: 'Exceptional work! The website is fantastic, and we appreciate your team\'s expertise and professionalism.' },
-  { id: '5', name: 'Aryan Goyal', company: 'thirtyml', logo: 'https://raw.githubusercontent.com/aryangoyal7/Frontend_trial/main/src/favicon.png', pic: '/aryan.png', star: '5', exp: 'It was very personalised and they did multiple iterations according to my requirements' },
+  {
+    id: "1",
+    name: "Eknoor Singh",
+    company: "Speezy",
+    logo: "https://media.licdn.com/dms/image/C4D0BAQFypz5GTWfe0g/company-logo_200_200/0/1673103068185?e=1704326400&v=beta&t=nogzLKRMtVsgYX9Y0tpFQXBliZhKMhzUBQWte3af91w",
+    pic: "/eknoor.png",
+    star: "5",
+    exp: "Great work so far, loved the fact that Eshu personally took it up! Looking forward to the final product",
+  },
+  {
+    id: "2",
+    name: "Abhishek Pai Angle",
+    company: "",
+    logo: "https://www.ribin.in/assets/SiteLogo-b873c477.png",
+    pic: "/abhishek.png",
+    star: "5",
+    exp: "Great work. They could exactly replicate the design into website and the speed was super quick. Eshu personally solved all the issue I had in quickest of time. Would definitely work with them again!",
+  },
+  {
+    id: "3",
+    name: "Sujas Jain",
+    company: "Abhyuday",
+    logo: "abhyuday",
+    pic: "",
+    star: "5",
+    exp: "Work execution was smooth and task was done on time!",
+  },
+  {
+    id: "4",
+    name: "Formula AshwaRiders",
+    company: "Formula Ashwariders",
+    logo: "https://formulaashwariders.in/assets/logo-e2d299f2.png",
+    pic: "https://raw.githubusercontent.com/Eshller/formulaashwariders/main/public/images/t7.jpeg",
+    star: "5",
+    exp: "Exceptional work! The website is fantastic, and we appreciate your team's expertise and professionalism.",
+  },
+  {
+    id: "5",
+    name: "Aryan Goyal",
+    company: "thirtyml",
+    logo: "https://raw.githubusercontent.com/aryangoyal7/Frontend_trial/main/src/favicon.png",
+    pic: "/aryan.png",
+    star: "5",
+    exp: "It was very personalised and they did multiple iterations according to my requirements",
+  },
 ];
