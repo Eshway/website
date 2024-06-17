8405f182d000bef500d24bb8ad1c804a7916eb90
Eshu Koli <eshller@Eshus-MacBook-Air.local>
2024-06-17 09:59:15 +0530

    apollo tracking 2

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,10 @@ const Home = () => (
     <Script src="https://www.googletagmanager.com/gtag/js?id=G-3MY67GFCTB" />
     <Script id="google-analytics">
       {`
+          function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+          o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+          o.onload=function(){window.trackingFunctions.onLoad({appId:"663f29bfa8928601c7633aee"})},
+          document.head.appendChild(o)}initApollo();
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
